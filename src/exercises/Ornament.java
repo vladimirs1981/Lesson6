3b2ed80fff0e23e3924cbd2fbd528b42f3111059
Vladimir Stojanovic <st.vladas@hotmail.com>
2019-05-26 17:56:47 +0200

    Ornament

--- src/exercises/Ornament.java
+++ src/exercises/Ornament.java
@@ -1,5 +1,6 @@
 package exercises;
 
+
 /* 
  * Create a class called Decorator that contains an ArrayList called boxOfDecorations.
  *   1. In the Decorator class, hang all the ornaments in the box.
@@ -7,9 +8,22 @@ package exercises;
  */
 
 public class Ornament {
-	
-	public void hang(){
-		System.out.println("hanging an ornament");
+
+	String color;
+
+	public Ornament(String color) {
+		super();
+		this.color = color;
+	}
+
+	public void hang() {
+		System.out.println("hanging a " + this.color + " ornament");
+	}
+
+	@Override
+	public String toString() {
+		// TODO Auto-generated method stub
+		return this.color;
 	}
 
 }

e7797374aef5d83c636c136f1b5d3ca0eb35cb6f
Vladimir Stojanovic <st.vladas@hotmail.com>
2019-06-02 17:42:24 +0200

    peace on earth

--- src/optional/PeaceOnEarth.java
+++ src/optional/PeaceOnEarth.java
@@ -1,6 +1,7 @@
 package optional;
 
 import java.util.ArrayList;
+import java.util.Objects;
 
 public class PeaceOnEarth {
 	public static void main(String[] args) {
@@ -21,7 +22,12 @@ public class PeaceOnEarth {
 		truth.add('k');
 
 		/* TODO: Remove the hash symbols and print out the truth. */
-		
-		
+		for (int i = 0; i < truth.size(); i++) {
+			if (Objects.equals('#', truth.get(i))) {
+				truth.remove(i);
+				i--;
+			}
+		}
+		System.out.println(truth.toString());
 	}
 }

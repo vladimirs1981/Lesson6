08bf6759b5143bea0561aa0cf82a15caa6e38af9
Vladimir Stojanovic <st.vladas@hotmail.com>
2019-06-02 18:54:59 +0200

    ArrayOverload

--- src/optional/ArrayOverload.java
+++ src/optional/ArrayOverload.java
@@ -1,7 +1,22 @@
 package optional;
 
+import java.util.ArrayList;
+import java.util.Random;
+
 /* Burst an ArrayList by putting so many things into it that you cause an OutOfMemory error. */
 
 public class ArrayOverload {
+
+	public static void main(String[] args) {
+
+		ArrayList<Integer> burst = new ArrayList<Integer>();
+
+		Random r = new Random();
+
+		while (true) {
+
+			burst.add(r.nextInt());
+		}
+	}
 
 }
